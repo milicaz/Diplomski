cb6fbe090f4785b1b225030580fc9faa63f7b56e
milicaz <zm025555@gmail.com>
2024-01-10 12:58:55 +0100

    Registracija i Login css

--- opens-frontend/src/pages/korisnik/Login.jsx
+++ opens-frontend/src/pages/korisnik/Login.jsx
@@ -2,29 +2,36 @@ import { Button, Card, Form } from "react-bootstrap";
 
 const Login = () => {
   return (
-    <Card>
-      <Card.Header>Login</Card.Header>
-      <Card.Body>
-        <Form>
-          <Form.Group>
-            <Form.Label>Email adresa</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Unesite vašu e-mail adresu"
-              required
-            />
-          </Form.Group>
-          <Form.Group>
-            <Form.Label>Lozinka</Form.Label>
-            <Form.Control type="password" placeholder="Unesite vašu lozinku" />
-          </Form.Group>
-          <Form.Group className="btn-reg">
-            <a href="">Zaboravili ste vašu lozinku</a>&nbsp;&nbsp;
-            <Button variant="success">Login</Button>
-          </Form.Group>
-        </Form>
-      </Card.Body>
-    </Card>
+    <div class="d-flex justify-content-center">
+      <Card style={{ width: "40%", height: "40%" }}>
+        <Card.Header>Login</Card.Header>
+        <Card.Body>
+          <Form>
+            <Form.Group>
+              <Form.Label>Email adresa</Form.Label>
+              <Form.Control
+                style={{ width: "60%", maxWidth: "70%" }}
+                type="text"
+                placeholder="Unesite vašu e-mail adresu"
+                required
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Lozinka</Form.Label>
+              <Form.Control
+                style={{ width: "60%", maxWidth: "70%" }}
+                type="password"
+                placeholder="Unesite vašu lozinku"
+              />
+            </Form.Group>
+            <Form.Group className="btn-reg">
+              <a href="">Zaboravili ste vašu lozinku</a>&nbsp;&nbsp;
+              <Button variant="success">Login</Button>
+            </Form.Group>
+          </Form>
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
 
--- opens-frontend/src/pages/korisnik/Registracija.jsx
+++ opens-frontend/src/pages/korisnik/Registracija.jsx
@@ -4,52 +4,60 @@ import ReactGridLayout from "react-grid-layout";
 
 const Registracija = () => {
   return (
-    <Card> 
-    <Card.Header>Registracija</Card.Header>
-    <Card.Body>
-    <Form>
-      <Form.Group>
-        <Form.Label>Ime i prezime</Form.Label>
-        <Form.Control type="text" required />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Email adresa</Form.Label>
-        <Form.Control type="email" required />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Lozinka</Form.Label>
-        <Form.Control
-          type="password"
-          aria-describedby="passwordHelpBlock"
-          required
-        />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Godine</Form.Label>
-        <Form.Control type="text" required />
-      </Form.Group>
-      <Form.Group>
-        <Form.Label>Broj telefona</Form.Label>
-        <Form.Control type="text" />
-      </Form.Group>
-      <Form.Group className="checkbox-reg">
-        {["checkbox"].map((type) => (
-          <div key={`default-${type}`} className="mb-3">
-            <Form.Check // prettier-ignore
-              type={type}
-              id={`default-${type}`}
-              // label={`default ${type}`}
-              label={"Potvrda podataka"}
-            />
-          </div>
-        ))}
-      </Form.Group>
-      <Form.Group>
-        <Button className="btn-reg" variant="success">Registruj se</Button>
-      </Form.Group>
-    </Form>
-    </Card.Body>
-    </Card>
+    <div class="d-flex justify-content-center">
+      <Card style={{ width: "40%", height: "40%" }}>
+        <Card.Header>Registracija</Card.Header>
+        <Card.Body>
+          <Form>
+            <Form.Group>
+              <Form.Label>Ime i prezime</Form.Label>
+              <Form.Control style={{ width: "60%", maxWidth: "70%" }}
+                type="text"
+                placeholder="Unesite vaše ime i prezime"
+                required
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Email adresa</Form.Label>
+              <Form.Control style={{ width: "60%", maxWidth: "70%" }} type="email" required />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Lozinka</Form.Label>
+              <Form.Control style={{ width: "60%", maxWidth: "70%" }}
+                type="password"
+                aria-describedby="passwordHelpBlock"
+                required
+              />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Godine</Form.Label>
+              <Form.Control style={{ width: "60%", maxWidth: "70%" }} type="text" required />
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Broj telefona</Form.Label>
+              <Form.Control style={{ width: "60%", maxWidth: "70%" }} type="text" />
+            </Form.Group>
+            <Form.Group className="checkbox-reg">
+              {["checkbox"].map((type) => (
+                <div key={`default-${type}`} className="mb-3">
+                  <Form.Check // prettier-ignore
+                    type={type}
+                    id={`default-${type}`}
+                    // label={`default ${type}`}
+                    label={"Potvrda podataka"}
+                  />
+                </div>
+              ))}
+            </Form.Group>
+            <Form.Group>
+              <Button className="btn-reg" variant="success">
+                Registruj se
+              </Button>
+            </Form.Group>
+          </Form>
+        </Card.Body>
+      </Card>
+    </div>
   );
 };
 
