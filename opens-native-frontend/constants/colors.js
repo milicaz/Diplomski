5c897252d7f09ee19c6d327a0de986b23f46d2f0
puskarovtam <tamara.puskarov@gmail.com>
2024-01-31 21:31:27 +0100

    Edit profile

--- opens-native-frontend/constants/colors.js
+++ opens-native-frontend/constants/colors.js
@@ -3,7 +3,8 @@ const COLORS = {
     black: "#222222",
     primary: "#6d39b6",
     secondary: "#b745d6",
-    grey: "#CCCCCC"
+    grey: "#CCCCCC",
+    secondaryGrey: "rgba(84, 76, 76, 0.14)"
 }
 
 export default COLORS;
--- opens-native-frontend/screens/ProfileEdit.js
+++ opens-native-frontend/screens/ProfileEdit.js
@@ -1,11 +1,227 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import COLORS from '../constants/colors'
+import { MaterialIcons } from '@expo/vector-icons';
+import React, { useState } from 'react';
+import { Image, ScrollView, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import COLORS from '../constants/colors';
 
-export default function ProfileEdit() {
+export default function ProfileEdit({ navigation }) {
+
+  const [name, setName] = useState("Jovana Jovanović");
+  const [email, setEmail] = useState("jovana.jovanovic@mail.com");
+  const [city, setCity] = useState("Novi Sad");
+  const [phone, setPhone] = useState("+381 61 2345678");
+  const [yearBirth, setYearBirth] = useState("2011");
+
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center", backgroundColor: COLORS.white }}>
-      <Text>ProfileEdit</Text>
-    </View>
+    <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white, paddingHorizontal: 22 }}>
+      <View style={{ flexDirection: "row", marginHorizontal: 12, justifyContent: 'center' }}>
+        <TouchableOpacity
+          onPress={() => navigation.goBack()}
+          style={{
+            position: "absolute",
+            left: 0,
+          }}
+        >
+          <MaterialIcons
+            name="keyboard-arrow-left"
+            size={24}
+            color={COLORS.black}
+          />
+        </TouchableOpacity>
+
+        <Text style={{ fontSize: 18 }}>Izmena profila</Text>
+      </View>
+      <ScrollView>
+        <View
+          style={{
+            alignItems: "center",
+            marginVertical: 22,
+          }}
+        >
+          <TouchableOpacity>
+            <Image
+              source={require("../assets/profile.png")}
+              style={{
+                height: 100,
+                width: 100,
+                borderRadius: 85,
+                borderWidth: 2,
+                borderColor: COLORS.primary,
+              }}
+            />
+
+            <View
+              style={{
+                position: "absolute",
+                bottom: 0,
+                right: 10,
+                zIndex: 9999,
+              }}
+            >
+              <MaterialIcons
+                name="photo-camera"
+                size={32}
+                color={COLORS.primary}
+              />
+            </View>
+          </TouchableOpacity>
+        </View>
+
+        <View
+          style={{
+            flexDirection: "column",
+            marginBottom: 6,
+          }}
+        >
+          <Text style={{ fontSize: 16 }}>Ime i prezime</Text>
+          <View
+            style={{
+              height: 44,
+              width: "100%",
+              borderColor: COLORS.secondaryGrey,
+              borderWidth: 1,
+              borderRadius: 4,
+              marginVertical: 6,
+              justifyContent: "center",
+              paddingLeft: 8,
+            }}
+          >
+            <TextInput
+              value={name}
+              onChangeText={(value) => setName(value)}
+              editable={true}
+            />
+          </View>
+        </View>
+
+        <View
+          style={{
+            flexDirection: "column",
+            marginBottom: 6,
+          }}
+        >
+          <Text style={{ fontSize: 16 }}>Email</Text>
+          <View
+            style={{
+              height: 44,
+              width: "100%",
+              borderColor: COLORS.secondaryGrey,
+              borderWidth: 1,
+              borderRadius: 4,
+              marginVertical: 6,
+              justifyContent: "center",
+              paddingLeft: 8,
+            }}
+          >
+            <TextInput
+              value={email}
+              onChangeText={(value) => setEmail(value)}
+              editable={true}
+            />
+          </View>
+        </View>
+
+        <View
+          style={{
+            flexDirection: "column",
+            marginBottom: 6,
+          }}
+        >
+          <Text style={{ fontSize: 16 }}>Mesto boravišta</Text>
+          <View
+            style={{
+              height: 44,
+              width: "100%",
+              borderColor: COLORS.secondaryGrey,
+              borderWidth: 1,
+              borderRadius: 4,
+              marginVertical: 6,
+              justifyContent: "center",
+              paddingLeft: 8,
+            }}
+          >
+            <TextInput
+              value={city}
+              onChangeText={(value) => setCity(value)}
+              editable={true}
+            />
+          </View>
+        </View>
+
+        <View
+          style={{
+            flexDirection: "column",
+            marginBottom: 6,
+          }}
+        >
+          <Text style={{ fontSize: 16 }}>Broj telefona</Text>
+          <View
+            style={{
+              height: 44,
+              width: "100%",
+              borderColor: COLORS.secondaryGrey,
+              borderWidth: 1,
+              borderRadius: 4,
+              marginVertical: 6,
+              justifyContent: "center",
+              paddingLeft: 8,
+            }}
+          >
+            <TextInput
+              value={phone}
+              onChangeText={(value) => setPhone(value)}
+              editable={true}
+            />
+          </View>
+        </View>
+
+        <View
+          style={{
+            flexDirection: "column",
+            marginBottom: 6,
+          }}
+        >
+          <Text style={{ fontSize: 16 }}>Godina rođenja</Text>
+          <View
+            style={{
+              height: 44,
+              width: "100%",
+              borderColor: COLORS.secondaryGrey,
+              borderWidth: 1,
+              borderRadius: 4,
+              marginVertical: 6,
+              justifyContent: "center",
+              paddingLeft: 8,
+            }}
+          >
+            <TextInput
+              value={yearBirth}
+              onChangeText={(value) => setYearBirth(value)}
+              editable={true}
+            />
+          </View>
+        </View>
+
+        <TouchableOpacity
+          style={{
+            backgroundColor: COLORS.primary,
+            height: 44,
+            borderRadius: 6,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 18,
+              color: COLORS.white,
+            }}
+          >
+            Sačuvaj izmene
+          </Text>
+        </TouchableOpacity>
+
+      </ScrollView>
+    </SafeAreaView >
   )
 }
