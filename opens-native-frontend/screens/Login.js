dec6f0a9ce5d12d5a4ca9e4c928f954a6e8bd10d
milicaz <zm025555@gmail.com>
2024-02-02 13:37:25 +0100

    Login stranica za mobilnu aplikaciju

--- opens-native-frontend/screens/Login.js
+++ opens-native-frontend/screens/Login.js
@@ -1,9 +1,21 @@
-import { Text, View } from "react-native";
+import { Button, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function Login() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Login page</Text>
+      <Text style={{ fontWeight: "bold", fontSize: 50, marginBottom: 40 }}>Login</Text>
+      <View style={{ width: "80%", borderWidth: 2, borderRadius: 25, height: 50, marginBottom: 20, justifyContent: "center", padding: 20 }}>
+        <TextInput style={{ height: 50, color: "black" }} placeholder="Email" />
+      </View>
+      <View style={{ width: "80%", borderWidth: 2, borderRadius: 25, height: 50, marginBottom: 20, justifyContent: "center", padding: 20 }}>
+        <TextInput style={{ height: 50, color: "black" }} placeholder="Password" />
+      </View>
+      <TouchableOpacity>
+        <Text style={{ fontSize: 18 }}>Zaboravili ste lozinku?</Text>
+      </TouchableOpacity>
+      <View style = {{ width:"50%", margin: 10 }}>
+        <Button title="Login"></Button>
+      </View>
     </View>
   );
 }
