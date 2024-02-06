6ee2d5cf206a97629bfd38261e58dc56a3d23502
milicaz <zm025555@gmail.com>
2024-02-06 18:11:08 +0100

    Registracija za mobilnu aplikaciju - datum

--- opens-native-frontend/screens/Registracije.js
+++ opens-native-frontend/screens/Registracije.js
@@ -1,5 +1,7 @@
+import RNDateTimePicker from "@react-native-community/datetimepicker";
 import { useState } from "react";
-import { Button, Text, TextInput, View } from "react-native";
+import { Button, Platform, Pressable, Text, TextInput, View } from "react-native";
+import DatePicker from "react-native-datepicker";
 export default function Registracija() {
 
   // State variable to hold the password 
@@ -11,6 +13,35 @@ export default function Registracija() {
   // Function to toggle the password visibility state 
   const toggleShowPassword = () => { 
       setShowPassword(!showPassword); 
+  }
+
+  const [date, setDate] = useState(new Date());
+  const [showPicker, setShowPicker] = useState(false);
+
+  const getDate = () => {
+    let tempDate = date.toString().split(' ');
+    return date !== ''
+      ? `${tempDate[0]} ${tempDate[1]} ${tempDate[2]} ${tempDate[3]}`
+      : '';
+  };
+
+  const toogleDatepicker = () => {
+    setShowPicker(!showPicker);
+  }
+
+  const onChange = ({type}, selectedDate) => {
+    if(type == "set") {
+      const currentDate = selectedDate;
+      setDate(currentDate);
+
+      if(Platform.OS === "android"){
+        toogleDatepicker();
+        setDate(currentDate);
+      }
+
+    } else {
+      toogleDatepicker();
+    }
   }
 
   return (
@@ -32,15 +63,15 @@ export default function Registracija() {
         <TextInput style = {{height: 50, color: "black"}} placeholder="Lozinka" />
       </View>
       <View style={{ width: "80%", borderWidth: 2, borderRadius: 25, height: 50, marginBottom: 20, justifyContent: "center", padding: 20 }}>
-        {/* <Button title="Open" onPress={() => setOpen(true)} />
-        <DatePicker modal open={open} date={date} onConfirm = {(date) => {
-          setOpen(false)
-          setDate(date)
-        }}
-          onCancel = {() => {
-            setOpen(false)
-          }}
-        /> */}
+        {showPicker && (
+          <RNDateTimePicker mode = "date" display = "default" value={date} onChange={onChange} />
+        )}
+        {!showPicker && (
+          <Pressable onPress={toogleDatepicker}>
+          <TextInput style = {{height: 50, color: "black"}} placeholder="Datum rodjenja" editable = {false} value={getDate()} onChangeText={setDate} />
+        </Pressable>
+        )}
+        
       </View>
       <View style={{ width: "80%", borderWidth: 2, borderRadius: 25, height: 50, marginBottom: 20, justifyContent: "center", padding: 20 }}>
         <TextInput style = {{height: 50, color: "black"}} placeholder="Mesto Boravista" />
