7e3708961cf049474df77d624fe953cf0704ce1a
milicaz <zm025555@gmail.com>
2024-02-27 15:17:56 +0100

    GET i POST metode za Mesto dogadjaja

--- opens-backend/src/main/java/com/opens/controller/MestoDogadjajaController.java
+++ opens-backend/src/main/java/com/opens/controller/MestoDogadjajaController.java
@@ -1,10 +1,40 @@
 package com.opens.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.opens.model.MestoDogadjaja;
+import com.opens.repository.MestoDogadjajaRepository;
 
 @RestController
 @RequestMapping("/api")
 public class MestoDogadjajaController {
+	
+	@Autowired
+	private MestoDogadjajaRepository mestoDogadjajaRepo;
+	
+	@GetMapping("/mestaDogadjaja")
+	public ResponseEntity<List<MestoDogadjaja>> getAll() {
+		List<MestoDogadjaja> mesta = new ArrayList<>();
+		mesta = mestoDogadjajaRepo.findAll();
+		
+		return new ResponseEntity<>(mesta, HttpStatus.OK);
+	}
+	
+	@PostMapping("/mestaDogadjaja")
+	public ResponseEntity<MestoDogadjaja> save(@RequestBody MestoDogadjaja mestoDogadjaja) {
+		MestoDogadjaja mesto = mestoDogadjajaRepo.save(mestoDogadjaja);
+		
+		return new ResponseEntity<>(mesto, HttpStatus.OK);
+	}
 
 }
